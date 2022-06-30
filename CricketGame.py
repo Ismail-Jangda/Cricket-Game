72c1d60dc27acfd3ddf731f05cf61640b013a168
Ismail-Jangda <46109732+Ismail-Jangda@users.noreply.github.com>
2022-06-30 02:04:36 -0400

    Increasing complexity
    Added wickets tally
    Realized class not required for bowling a delivery, so changed it to just a function.
    Added bowling an over - which calls on deliveries 6 times. I need 20 overs for a T20 innings

--- CricketGame.py
+++ CricketGame.py
@@ -2,7 +2,8 @@
 
 import random
 import math
-score = 0 
+score = 0
+wickets = 0 
 class player:
     def __init__ (self, name = "Player_Name", Bat_Skill=0, Bowl_Skill=0, Field_Skill = 0):
         self.name = name
@@ -18,57 +19,67 @@ class player:
         Bowling_Points = self.Bowl_Skill + random.randint(-50,50)
         return Bowling_Points
 
-class Delivery:
-    def bowl_delivery(batsman, bowler):
-        global score
-        batsman_points = batsman.Batting()
-        bowler_points = bowler.Bowling()
-        point_differential = batsman_points - bowler_points
-        if point_differential >= 80:
-            score += 6
-            print ("Batsman has scored 6 runs")
-        elif point_differential >= 60:
-            score += 4
-            print("Batsman has scored 4 runs")
-        elif point_differential >= 50:
-            score +=3
-            print("Batsman has scored 3 runs")
-        elif point_differential >= 30:
-            score +=2
-            print("Batsman has scored 2 runs")
-        elif point_differential >= 0:
-            score +=1
-            print("Batsman has scored 1 run")
-        elif point_differential >= -30:
-            print("Dot ball. No runs added.")
-        elif point_differential <= -80: 
-            print("Wicket! Taken! {} has bowled a wonderful delivery and has completely and utterly bamboozled {}".format(bowler.name, batsman.name)) 
-        elif point_differential <= -60:
-            wicket_chance = random.randint(-40,60)
-            if wicket_chance >= 0:
-                print("Loud appeal for an LBW, looks plumb... AND ITS GIVEN! {} heads back to the pavilion".format(batsman.name))
-            else:
-                print("The bowling side have all gone up to appeal this, but not given. Umpire indicating that the delivery was a bit high.") 
-        elif point_differential <= -50:
-            wicket_chance = random.randint(-50,50)
-            if wicket_chance >= 0:
-                print("Edged, and TAKEN! Excellent catch by the wicket keeper! {} shaking his head in disgust. What an unnecessary dismissal".format(batsman.name))
-            else:
-                print("DROPPED! Regulation catch dropped by the keeper. {} looks distraught. That really should be taken at this level of game play.".format(bowler.name))             
-        elif point_differential <= -31:
-            wicket_chance = random.randint(-60,40)
-            if wicket_chance >= 0:
-                print("{} slashes at the ball, straight into the hands of the fielder. Lucky breakthrough by {}".format(batsman.name, bowler.name))
-            else:
-                print("The fielder shelled the chance there. {} put alot of power in that shot, and the fielder was unable to grab onto it cleanly. Dot ball".format(batsman.name, bowler.name))     
+def bowl_delivery(batsman, bowler):
+    global score
+    global wickets
+    batsman_points = batsman.Batting()
+    bowler_points = bowler.Bowling()
+    point_differential = batsman_points - bowler_points
+    if point_differential >= 80:
+        score += 6
+        print ("Batsman has scored 6 runs")
+    elif point_differential >= 60:
+        score += 4
+        print("Batsman has scored 4 runs")
+    elif point_differential >= 50:
+        score +=3
+        print("Batsman has scored 3 runs")
+    elif point_differential >= 30:
+        score +=2
+        print("Batsman has scored 2 runs")
+    elif point_differential >= 0:
+        score +=1
+        print("Batsman has scored 1 run")
+    elif point_differential >= -30:
+        print("Dot ball. No runs added.")
+    elif point_differential <= -80: 
+        print("Wicket! Taken! {} has bowled a wonderful delivery and has completely and utterly bamboozled {}".format(bowler.name, batsman.name)) 
+        wickets += 1
+    elif point_differential <= -60:
+        wicket_chance = random.randint(-40,60)
+        if wicket_chance >= 0:
+            print("Loud appeal for an LBW, looks plumb... AND ITS GIVEN! {} heads back to the pavilion".format(batsman.name))
+            wickets += 1
+        else:
+            print("The bowling side have all gone up to appeal this, but not given. Umpire indicating that the delivery was a bit high.") 
+    elif point_differential <= -50:
+        wicket_chance = random.randint(-50,50)
+        if wicket_chance >= 0:
+           print("Edged, and TAKEN! Excellent catch by the wicket keeper! {} shaking his head in disgust. What an unnecessary dismissal".format(batsman.name))
+           wickets += 1
+        else:
+            print("DROPPED! Regulation catch dropped by the keeper. {} looks distraught. That really should be taken at this level of game play.".format(bowler.name))             
+    elif point_differential <= -31:
+        wicket_chance = random.randint(-60,40)
+        if wicket_chance >= 0:
+            print("{} slashes at the ball, straight into the hands of the fielder. Lucky breakthrough by {}".format(batsman.name, bowler.name))
+            wickets += 1
+        else:
+            print("The fielder shelled the chance there. {} put alot of power in that shot, and the fielder was unable to grab onto it cleanly. Dot ball".format(batsman.name, bowler.name))     
 
-
+def bowl_over(batsman, bowler):
+    for x in range(6):
+        if wickets == 10:
+            print("This closes the innings. The batting team was able to score {}".format(score))
+            return
+        print("Over: 0.{}".format(x+1) )
+        bowl_delivery(batsman, bowler)
+    print("At the end of the over, the score is {} for {}".format(score, wickets))
 
 
 def main():
     Wasim = player("Wasim Akram", 70, 95, 75)
     Sachin = player("Sachin Tendulkar", 95, 63, 70)
-    Game = Delivery()
-    Delivery.bowl_delivery( Wasim, Sachin)
+    bowl_over(Sachin, Wasim)
 
 main()
---
+++ Teams and players.py
@@ -0,0 +1,37 @@
+
+class Team:
+    def __init__ (self, name = "Player_Name", Bat_Skill=0, Bowl_Skill=0, Field_Skill = 0, Fitness=0, Batting_Order=0, Playing_Role=""):
+        self.name = name
+        self.Bat_Skill = Bat_Skill
+        self.Bowl_Skill = Bowl_Skill
+        self.Field_Skill = Field_Skill  
+        self.Fitness = Fitness 
+        self.Batting_Order = Batting_Order
+        self.Playing_Role = Playing_Role
+
+    Team_Pakistan=[ 
+        ["Saeed Anwar", 84, 25, 80, 100, 1, "Spin"],
+        ["Zaheer Abbas", 90, 10, 85, 90, 2, "Spin"],
+        ["Babar Azam", 95, 35, 94, 95, 3, "Spin"],
+        ["Javed Miandad", 87, 10, 75, 85, 4, "Spin"],
+        ["Inzamam ul-Haq", 89, 0, 85, 70, 5, "Spin"],
+        ["Muhammad Rizwan", 75, 0, 100, 100, 6, "Keep"],
+        ["Imran Khan", 75, 95, 95, 95, 7, "Fast"],
+        ["Wasim Akram", 70, 95, 95, 95, 8, "Fast"],
+        ["Saqlain Mushtaq", 35 , 92, 75, 80, 9, "Spin"],
+        ["Waqar Younis", 25, 93, 85, 95, 10, "Fast"],
+        ["Saeed Ajmal", 20, 93, 65, 85, 11, "Spin"]
+    ]
+    Team_India=[ 
+        ["Rohit Sharma", 87, 25, 85, 90, 1, "Spin"],
+        ["Sachin Tendulkar", 95, 45, 75, 90, 2, "Spin"],
+        ["Sourav Ganguly", 77, 55, 85, 90, 3, "Fast"],
+        ["Virat Kohli", 95, 10, 95, 100, 4, "Spin"],
+        ["Yuvraj Singh", 80, 65, 90, 85, 5, "Spin"],
+        ["MS Dhoni", 85, 0, 100, 100, 6, "Keep"],
+        ["Kapil Dev", 85, 80, 95, 95, 7, "Fast"],
+        ["Zaheer Khan", 25, 85, 80, 90, 8, "Fast"],
+        ["Harbajan Singh", 40 , 75, 65, 80, 9, "Spin"],
+        ["Muhammad Shami", 15, 90, 80, 90, 10, "Fast"],
+        ["Anil Kumble", 35, 85, 70, 85, 11, "Spin"]
+    ]
